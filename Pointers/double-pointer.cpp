5b95ce1936bbd932afa8529648fdb52c70c447a6
huzaifa-anw <anwarhuzaifa226@gmail.com>
2025-09-03 21:29:53 +0500

    implemented a double pointer de-referencing

--- Pointers/double-pointer.cpp
+++ Pointers/double-pointer.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
-    
+    int num = 55; // variable 
+    int *ptrNum = &num;
+    int **ptrPtrNum = &ptrNum;
+
+    cout << **ptrPtrNum << endl;
+
     return 0;
 }
