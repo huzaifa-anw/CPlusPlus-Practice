aa072e48b5621a0fb52c86eb06a79810d2fc323e
huzaifa-anw <anwarhuzaifa226@gmail.com>
2025-09-09 16:13:44 +0500

    implemented abstraction through an abstract class

---
+++ OOP/abstraction.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Employee {
+protected:
+    string name;
+    int yearsAffiliated;
+    int rating;
+
+public: 
+    // constructors
+    Employee() = default;
+    Employee(string n, int y, int r) : name(n), yearsAffiliated(y), rating(r) {};
+    //abstract function
+    virtual void askForPromotion() = 0;
+
+    // getters 
+    string getName(){
+        return name;
+    }
+
+    int getYearsAffiliated() {
+        return yearsAffiliated;
+    }
+
+    int getRating() {
+        return rating;
+    }
+};
+
+class AmazonEmployee : public Employee{    
+
+public:
+    using Employee::Employee;
+    void askForPromotion() override {
+        int yearsAffiliated = getYearsAffiliated();
+        if (yearsAffiliated > 2) {
+            cout << getName() << " has been promoted" << endl;
+        }
+        else {
+            cout << "sorry, " << getName() << " can not be promoted" << endl;
+        }
+    }
+};
+
+class MetaEmployee : public Employee {
+
+public:
+    using Employee::Employee;
+    void askForPromotion() override {
+        int rating = getRating();
+        if (rating >= 8) {
+            cout << getName() << " has been promoted" << endl;
+        }
+        else {
+            cout << "sorry, " << getName() << " can not be promoted" << endl;
+        }
+    }
+};
+
+int main() {
+    AmazonEmployee e1("huzaifa", 5, 8);
+    e1.askForPromotion();
+
+    MetaEmployee e2("umar", 2, 8);
+    e2.askForPromotion();
+
+    MetaEmployee e3("ahmed", 2, 7);
+    e3.askForPromotion();
+}
