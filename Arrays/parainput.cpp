6f53e04273e9dbe0f58275c7fd94fbc010ccfed8
huzaifa-anw <anwarhuzaifa226@gmail.com>
2025-11-12 19:05:05 +0500

    paragraph input

---
+++ Arrays/parainput.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    const int SIZE = 100;
+    char str[SIZE];
+    cout << "enter a paragraph" << endl ;
+    cin.get(str, SIZE, '!');
+    cout << str;
+
+    return 0;
+}
